9512d5d0bb466166ad23e500eda487376c503b72
frankievx <frankievx@gmail.com>
2016-08-02 18:10:42 -0500

    Set up index for express server

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+import * from 'babel/register'({});
+
+
+import server from './server';
+const PORT = process.env.PORT || 3000;
+
+
+server.listen(PORT, function () {
+  console.log('Server listening on', PORT);
+});
