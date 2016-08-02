2167208b70e99114f807c5e1511595147001b418
frankievx <frankievx@gmail.com>
2016-08-02 18:16:09 -0500

    Build Blank HTML web page served through Express

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 import * from 'babel/register'({});
+import server from './server/server';
 
 
-import server from './server';
 const PORT = process.env.PORT || 3000;
 
 
 server.listen(PORT, function () {
   console.log('Server listening on', PORT);
 });
+
