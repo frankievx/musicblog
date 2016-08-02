4d23fcd07861729000ba83c4f84eaf1787ec9baa
frankievx <frankievx@gmail.com>
2016-08-02 18:57:18 -0500

    Hooked up server side rendering

--- shared/containers/App.js
+++ shared/containers/App.js
@@ -5,8 +5,6 @@ export default class AppView extends React.Component {
   render() {
     return (
       <div id="app-view">
-        <h1>Todos</h1>
-        <hr />
         {this.props.children}
       </div>
     );
