985e38721776c486bb12c5d86622fee9a818cecb
frankievx <frankievx@gmail.com>
2016-08-12 14:40:39 -0500

    Add database data and fix request for article data

--- src/redux/api/requestArticles.js
+++ src/redux/api/requestArticles.js
@@ -2,8 +2,11 @@ import axios from 'axios';
 
 
 const port = 3030;
-const url = 'http://localhost:' + port;
-
+const url = if (process.env.NODE_ENV !== 'production') { 
+	return 'http://localhost:' + port; 
+} else {
+	return 'http://www.frankiexv.com/api';
+}
 export default function requestArticles() {
 	return axios.get(url + '/articles');
 }
