95c72b133905d47057923cd9e59b217af525f814
frankievx <frankievx@gmail.com>
2016-08-13 16:46:12 -0500

    Add tests and edited prod script in package.json

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,3 +1,3 @@
-var context = require.context('./api', true, /-test\.js$/);
+var context = require.context('./mongo_server', true, /-test\.js$/);
 // var apicontext = require.context('./api', true, /-test\.js$/);
 context.keys().forEach(context);
