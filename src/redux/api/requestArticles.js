e0e90ac7a4513fdc575b5a266539f61b5467ce3b
frankievx <frankievx@gmail.com>
2016-08-25 01:34:48 -0700

    production server api change

--- src/redux/api/requestArticles.js
+++ src/redux/api/requestArticles.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const port = 3030;
-const url = __DEVELOPMENT__ ? 'http://localhost:' + port : 'http://www.frankiexv.com/api';
+const url = __DEVELOPMENT__ ? 'http://localhost:' + port : 'http://blog.frankiexv.com/api';
 
 export default function requestArticles() {
 	return axios.get(url + '/articles');
